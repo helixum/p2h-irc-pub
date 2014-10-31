8aeec3b545a09ce7f5ea83deb24951a9b785421b
helixum <hendrikstal@gmail.com>
2014-10-31 20:04:58 +0100

    fixing mismatch in alpha.sh

--- alpha.sh
+++ alpha.sh
@@ -2,3 +2,15 @@
 . $PWD/options.cfg
 echo $setupdir
 echo $Unrealdir
+if test -d $Unrealdir; then
+echo backup files for later useage
+if test -d $setupdir/backup; then
+cp $Unrealdir/*.conf $setupdir/backup
+cp $Unrealdir/ircd.* $setupdir/backup; else 
+echo backup not found in $setupdir
+echo i will create one
+mkdir $setupdir/backup
+cp $Unrealdir/*.conf $setupdir/backup
+cp $Unrealdir/ircd.* $setupdir/backup; fi 
+echo Backup done; else 
+echo $Unrealdir not found can not make backups;fi 
