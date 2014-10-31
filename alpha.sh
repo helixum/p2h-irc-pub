3d33bbbb79e3375a29531b31830ae4d091b8ab15
helixum <hendrikstal@gmail.com>
2014-10-31 19:09:57 +0100

    upgrade the repos on github

--- alpha.sh
+++ alpha.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/bash
 if test -e $PWD/version; then
-echo UnrealSetup `cat $PWD/version`; else echo No version file found in $PWD; exit ;fi 
+echo UnrealSetup `cat $PWD/version`; 
+if test -d $HOME/Unreal3.2; then
+echo er bestaat al een Unreal3.2
+echo check welke version het is
+if test -e $HOME/Unreal3.2/version then
+if test `cat $HOME/Unreal3.2/version` = 3.2.10.4;then
+echo Geen upgrade nodig ; exit ; else 
+echo o dear u heeft nog een oudere versie
+echo Laatste en meest recente versie van UnrealIRCD is 3.2.10.4 
+echo gevonden versie is `cat $HOME/Unreal3.2/version`
+echo 
+
+else echo No version file found in $PWD; exit ;fi 
